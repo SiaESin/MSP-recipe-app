9dafff1581fcdb1b04a008868f0cf5bbbe220d41
MFoster17 <ausslanderofficial@gmail.com>
2023-08-16 02:36:23 -0600

    finshed routes and models

--- server/src/index.js
+++ server/src/index.js
@@ -7,4 +7,7 @@ const app = express()
 app.use(express.json());
 app.use(cors());
 
+mongoose.connect("mongodb+srv://siabhons:Ywz6WC2UAGMzXvbp@recipes.ek7x2jb.mongodb.net/recipes")
+
+
 app.listen(3001, () => console.log("SERVER STARTED!"))
