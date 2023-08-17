405c0b9616dad0d2c04564b260d8cdd9d2585c83
MFoster17 <ausslanderofficial@gmail.com>
2023-08-16 18:36:13 -0600

    finalized index.js for server

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,23 @@
 import express from "express";
-import cors from 'cors'
-import mongoose from 'mongoose'
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/user.js";
+import { recipesRouter } from "./routes/recipes.js";
 
-const app = express()
+const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb+srv://siabhons:Ywz6WC2UAGMzXvbp@recipes.ek7x2jb.mongodb.net/recipes")
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
+
+mongoose.connect("mongodb+srv://siabhons:Ywz6WC2UAGMzXvbp@recipes.ek7x2jb.mongodb.net/recipes",
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}
+);
 
 
 app.listen(3001, () => console.log("SERVER STARTED!"))
