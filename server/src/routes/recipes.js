a2a9618301d494677e7e517776e7bae99583bff1
SiaESin <siabhon.sindab@gmail.com>
2023-08-16 18:33:55 -0700

    adding saved to recipes

--- server/src/routes/recipes.js
+++ server/src/routes/recipes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import { RecipeModel } from "../models/Recipes.js";
+import { UserModel } from "../models/Users.js";
 
 const router = express.Router();
 
@@ -23,4 +24,36 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.put('/', async (req, res) => {
+  try{
+    const recipe = await RecipeModel.findById(req.body.recipeID)
+    const user = await UserModel.findById(req.body.userID)
+    user.savedRecipes.push(recipe);
+    await user.save();
+    res.json({ savedRecipes: user.savedRecipes });
+  } catch (e) {
+      res.json(e);
+  }
+});
+
+router.get('/savedRecipes/ids', async (req, res) => {
+    try {
+        const user = await UserModel.findById(req.body.userID)
+        res.json({ savedRecipes: user?.savedRecipes })
+      } catch (e) {
+        res.json(e)
+    }
+})
+
+router.get('/savedRecipes/', async (req, res) => {
+  try {
+      const user = await UserModel.findById(req.body.userID)
+      const savedRecipes = await RecipeModel.find({
+        _id: { $in: user.savedRecipes },
+      })
+      res.json({ savedRecipes })
+  } catch (e) {
+      res.json(e)
+  }
+})
 export { router as recipesRouter };
